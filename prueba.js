2c116dfa7f06d1797d3ed4a52450b673c7bee574
PaulaJimenez7 <paulajm@usal.es>
2023-06-22 15:47:26 +0200

    Primera practica

---
+++ prueba.js
@@ -0,0 +1,13 @@
+var libro = {
+    titulo: 'Las',
+    autor: 'Santi',
+    informacion:function(){return this.titulo + " escrito por " + this.autor;}
+
+}
+
+console.log(typeof libro.informacion);
+
+<script type="text/javascript">
+    let intentos =[1,5,10,12]
+    console.log(intentos[4])
+</script>
